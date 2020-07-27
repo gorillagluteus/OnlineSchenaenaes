aa4a9846695cfec43dd50a8e763bf556663959fa
sid <52465526+gorillagluteus@users.noreply.github.com>
2020-07-27 09:48:57 -0400

    Initial Push~
    hashtag swag

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float sensitivity = 100f;
+    public float yOffset;
+    public Transform playerBody;
+    private bool camUpdate = false;
+    private float xRotation = 0f;
+    void Update()
+    {
+        //Cursor.lockState = CursorLockMode.Locked;
+        if (playerBody != null)
+        {
+            this.transform.position = playerBody.position + new Vector3(0,yOffset,0);
+        }
+        if (camUpdate)
+        {
+            MouseAiming();
+        }
+    }
+
+    void MouseAiming()
+    {
+        // get the mouse inputs
+        float x = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float y = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRotation -= y;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * x);
+    }
+    public void UpdatePlayerBody(Transform t)
+    {
+        camUpdate = true;
+        this.playerBody = t;
+        this.transform.position = new Vector3(0, 0, 0);
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using Mirror;
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float smoothing;
+
+
+    public CharacterController c;
+
+    private int multiplierX;
+    private int multiplierZ;
+    private bool diagonalCheck;
+    private float turn;
+    private NetworkIdentity id;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameObject MCam = GameObject.FindGameObjectWithTag("Cameras");
+        MCam.transform.parent = this.transform;
+        MCam.GetComponent<CameraControl>().UpdatePlayerBody(this.transform);
+        id = this.gameObject.GetComponent<NetworkIdentity>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (id.hasAuthority)
+        {
+            float x = Input.GetAxisRaw("Horizontal");
+            float z = Input.GetAxisRaw("Vertical");
+            FPS(x, z);
+        }
+    }
+    void FPS(float x, float z)
+    {
+        Vector3 move = transform.right * x + transform.forward * z;
+        c.Move(move * speed * Time.deltaTime);
+    }
+}
